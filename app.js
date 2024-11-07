e5a52143eefbd429129fa2cf09ebeda3b2e183b0
joao <141145414+nuneshenrique@users.noreply.github.com>
2024-11-07 11:59:59 -0300

    ok

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('article')
+    criaCartao.class ='cartao'
+    criaCartao.innerHTML = 
+    <div class="cartao_conteudo">
+<h3>${categoria}</h3>
+    </div class="cartao_conteudo_pergunta">
+    <p>${pergunta}</p>
+
 }
