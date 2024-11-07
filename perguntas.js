e30bd3d05a3759f6b42754f9d701216dca46fa14
joao <141145414+nuneshenrique@users.noreply.github.com>
2024-11-07 11:47:31 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,28 @@ criaCartao(
     'O que é python?',
     'o Python é uma linguagem de programação'
 )
+criaCartao(
+    'programação',
+    'O que é python?',
+    'o Python é uma linguagem de programação'
+)
+criaCartao(
+    'programação',
+    'O que é python?',
+    'o Python é uma linguagem de programação'
+)
+criaCartao(
+    'programação',
+    'O que é python?',
+    'o Python é uma linguagem de programação'
+)
+criaCartao(
+    'programação',
+    'O que é python?',
+    'o Python é uma linguagem de programação'
+)
+criaCartao(
+    'programação',
+    'O que é python?',
+    'o Python é uma linguagem de programação'
+)
